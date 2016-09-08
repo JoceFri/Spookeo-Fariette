520ddca8e11176f84e0ed5bc43653f4d04682fed
hikari1734 <mlwalthe@mtu.edu>
2016-09-08 19:53:43 -0400

    Add files via upload
    Window and start button javafx

--- Platformer/src/Platformer.java
+++ Platformer/src/Platformer.java
@@ -1,4 +1,36 @@
-import javafx.*;
-public class Platformer {
+package hgd;
 
+//imports
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Platformer extends Application {
+	 public static void main(String[] args) {
+	        launch(args);
+	    }
+@Override
+public void start(Stage primaryStage) {
+    primaryStage.setTitle("Start");
+    Button btn = new Button();
+    btn.setText("Start");
+    btn.setOnAction(new EventHandler<ActionEvent>() {
+
+    	//action for the start button
+        @Override
+        public void handle(ActionEvent event) {
+           
+        }
+    });
+    
+    StackPane root = new StackPane();
+    root.getChildren().add(btn);
+    primaryStage.setScene(new Scene(root, 500, 500));
+    primaryStage.show();
+}
 }
