739993a66adb4ff49f4305c89e7b13ddd410f4e0
hikari1734 <mlwalthe@mtu.edu>
2016-09-08 20:49:30 -0400

    Add files via upload
    rectangle added and start button disappears to draw rectangle. movement attempted

--- Platformer/src/Platformer.java
+++ Platformer/src/Platformer.java
@@ -6,31 +6,76 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 
 public class Platformer extends Application {
+	
 	 public static void main(String[] args) {
 	        launch(args);
 	    }
-@Override
-public void start(Stage primaryStage) {
-    primaryStage.setTitle("Start");
-    Button btn = new Button();
+	 
+	 private static final int movement = 5;
+	 //creates a new window pane
+	 Pane root = new Pane();
+	 
+//creates shape that is added when button pressed
+private Rectangle makeRectangle(){
+	Rectangle r1 = new Rectangle(50,25,100,140);
+    r1.setStroke(Color.BLACK);
+    r1.setFill(null);
+    r1.setStrokeWidth(3);
+	return r1;
+}
+
+//creates start button
+private Button startButton(){
+	Button btn = new Button();
     btn.setText("Start");
+    btn.setTranslateX(100);
+    btn.setTranslateY(200);
+    
     btn.setOnAction(new EventHandler<ActionEvent>() {
 
     	//action for the start button
+    	//sets button to false and creates a rectangle that appears after
         @Override
         public void handle(ActionEvent event) {
-           
+           btn.setVisible(false);
+           root.getChildren().add(makeRectangle());
         }
     });
+    return btn;
+}
+
+//makes shape move....
+private void moveRectangleOnKeyPress(Pane root, final Rectangle rectangle) {
+    root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+      @Override public void handle(KeyEvent event) {
+        switch (event.getCode()) {
+          case UP:    rectangle.setTranslateY(movement + 100); break;
+          case RIGHT: rectangle.setTranslateX(movement + 100); break;
+          case DOWN:  rectangle.setTranslateY(movement - 100); break;
+          case LEFT:  rectangle.setTranslateX(movement - 100); break;
+        }
+      }
+    });
+  }
+
+//sets scene and adds objects
+@Override
+public void start(Stage primaryStage) {
+    primaryStage.setTitle("Start");
+ 
     
-    StackPane root = new StackPane();
-    root.getChildren().add(btn);
+    root.getChildren().add(startButton());
+
     primaryStage.setScene(new Scene(root, 500, 500));
+    moveRectangleOnKeyPress(root, makeRectangle());
     primaryStage.show();
 }
 }
