5fdd4f300bbe3d39c24f24ae5aecf28689b1cd21
hikari1734 <mlwalthe@mtu.edu>
2016-09-11 20:45:42 -0400

    Add files via upload
    added image for the icon hero

--- Platformer/src/Platformer.java
+++ Platformer/src/Platformer.java
@@ -4,8 +4,11 @@ package hgd;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -14,68 +17,86 @@ import javafx.stage.Stage;
 
 
 public class Platformer extends Application {
-	
-	 public static void main(String[] args) {
-	        launch(args);
-	    }
-	 
-	 private static final int movement = 5;
-	 //creates a new window pane
-	 Pane root = new Pane();
-	 
-//creates shape that is added when button pressed
-private Rectangle makeRectangle(){
-	Rectangle r1 = new Rectangle(50,25,100,140);
-    r1.setStroke(Color.BLACK);
-    r1.setFill(null);
-    r1.setStrokeWidth(3);
-	return r1;
-}
 
-//creates start button
-private Button startButton(){
-	Button btn = new Button();
-    btn.setText("Start");
-    btn.setTranslateX(100);
-    btn.setTranslateY(200);
-    
-    btn.setOnAction(new EventHandler<ActionEvent>() {
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    	//action for the start button
-    	//sets button to false and creates a rectangle that appears after
-        @Override
-        public void handle(ActionEvent event) {
-           btn.setVisible(false);
-           root.getChildren().add(makeRectangle());
-        }
-    });
-    return btn;
-}
+	private static final int movement = 5;
+	//creates a new window pane
+	Pane root = new Pane();
 
-//makes shape move....
-private void moveRectangleOnKeyPress(Pane root, final Rectangle rectangle) {
-    root.setOnKeyPressed(new EventHandler<KeyEvent>() {
-      @Override public void handle(KeyEvent event) {
-        switch (event.getCode()) {
-          case UP:    rectangle.setTranslateY(movement + 100); break;
-          case RIGHT: rectangle.setTranslateX(movement + 100); break;
-          case DOWN:  rectangle.setTranslateY(movement - 100); break;
-          case LEFT:  rectangle.setTranslateX(movement - 100); break;
-        }
-      }
-    });
-  }
+	//creates shape that is added when button pressed
+	private Rectangle makeRectangle(){
+		Rectangle r1 = new Rectangle(50,25,100,140);
+		r1.setStroke(Color.BLACK);
+		r1.setFill(null);
+		r1.setStrokeWidth(3);
+		return r1;
+	}
 
-//sets scene and adds objects
-@Override
-public void start(Stage primaryStage) {
-    primaryStage.setTitle("Start");
- 
-    
-    root.getChildren().add(startButton());
+	//creates start button
+	private Button startButton(){
+		Button btn = new Button();
+		btn.setText("Start");
+		btn.setTranslateX(100);
+		btn.setTranslateY(200);
 
-    primaryStage.setScene(new Scene(root, 500, 500));
-    moveRectangleOnKeyPress(root, makeRectangle());
-    primaryStage.show();
-}
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+
+			//action for the start button
+			//sets button to false and creates a rectangle that appears after
+			@Override
+			public void handle(ActionEvent event) {
+				btn.setVisible(false);
+				root.getChildren().add(makeRectangle());
+			}
+		});
+		return btn;
+	}
+
+
+
+	//makes shape move....
+	private void moveRectangleOnKeyPress(Pane root, final Rectangle rectangle) {
+		root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override public void handle(KeyEvent event) {
+				switch (event.getCode()) {
+				case UP:    rectangle.setTranslateY(movement + 100); break;
+				case RIGHT: rectangle.setTranslateX(movement + 100); break;
+				case DOWN:  rectangle.setTranslateY(movement - 100); break;
+				case LEFT:  rectangle.setTranslateX(movement - 100); break;
+				}
+			}
+		});
+	}
+	//adds picture for sprite
+	private ImageView image(){
+		String Images =
+				"file:///Users/marissawalther/Desktop/desktop/triforce.png";
+		Image heroImage;
+		heroImage = new Image(Images, 70, 60, false, false);
+		ImageView hero = new ImageView(heroImage);
+
+		hero = new ImageView(heroImage);
+		hero.setTranslateX(50);
+		hero.setTranslateY(50);
+		return hero;
+	}
+
+
+	//sets scene and adds objects
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Start");
+
+		//  
+
+
+		root.getChildren().add(startButton());
+		root.getChildren().add(image());
+		primaryStage.setScene(new Scene(root, 500, 500));
+		moveRectangleOnKeyPress(root, makeRectangle());
+		primaryStage.show();
+	}
 }
