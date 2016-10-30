5b76f0d130ed49a0b7ddb4b92b17ef707343859e
cwalker97 <collinw@mtu.edu>
2016-10-30 18:44:41 -0400

    collision start

--- Platformer/src/Collision.java
+++ Platformer/src/Collision.java
@@ -1,15 +1,44 @@
-import javafx.scene.shape.Shape;
+
 
 public class Collision {
+	Actor shapeOne = null;
+	Actor shapeTwo = null;
+	private boolean leftCollision;
+	private boolean rightCollision;
+	public Collision(Actor one, Actor two){
+		shapeOne = one;
+		shapeTwo = two;
+	}
+	public boolean isColliding(){
+				System.out.println(shapeOne.getHBX() + " " + shapeTwo.getHBX());
+				if( shapeOne.getHBX()+shapeOne.getHBWidth() <= shapeTwo.getHBX() 
+					&& shapeOne.getHBY() >= shapeTwo.getHBY() 
+					&& (shapeTwo.getHBX() - (shapeOne.getHBX()+shapeOne.getHBWidth())) <= Platformer.acceleration){
+						//System.out.println("TRUE");	
+						leftCollision = true;
+						return true;
+						}
+				if( shapeOne.getHBX() >= shapeTwo.getHBX() + shapeTwo.getHBWidth()
+					&& shapeOne.getHBY() >= shapeTwo.getHBY()
+					&& shapeOne.getHBX() - (shapeTwo.getHBX() + shapeTwo.getHBWidth()) <= Platformer.acceleration){
+						System.out.println("TRUE");
+						rightCollision = true;
+						return true;
+				}
+					
+		return false;
+	}
 	
-	
-	
-	public int isColliding(Shape one, Shape two){
-		if (one.equals(two)) {
-			return 0;
-		}
-		else {
-			return 1;
+	public void moveObject(){
+		if(isColliding()){
+			if(leftCollision){
+				shapeTwo.setX(shapeTwo.getX() + Platformer.acceleration);
+				shapeTwo.setHBX(shapeTwo.getHBX() + Platformer.acceleration);
+			}
+			if(rightCollision){
+				shapeTwo.setX(shapeTwo.getX() - Platformer.acceleration);
+				shapeTwo.setHBX(shapeTwo.getHBX() - Platformer.acceleration);
+			}
 		}
 	}
 }
