1244373693555fbe8d73feb39c2fcc3b858a3832
Codey Walker <codeyw@mtu.edu>
2017-02-26 18:54:20 -0500

    Added absolutex variable to player

--- Platformer/src/Player.java
+++ Platformer/src/Player.java
@@ -2,8 +2,19 @@ import javafx.scene.image.ImageView;
 
 public class Player extends Actor {
 
+	double absoluteX = 0;
 	public Player(double ex, double why, int sizeX, int sizeY, ImageView person, double hbx, double hby, int hbwidth,
 			int hblength) {
 		super(ex, why, sizeX, sizeY, person, hbx, hby, hbwidth, hblength);
+		absoluteX = hbx;
+	}
+	
+	
+	public void setAbsX (double ex) {
+		absoluteX = ex;
+	}
+	
+	public double getAbsX() {
+		return absoluteX;
 	}
 }
