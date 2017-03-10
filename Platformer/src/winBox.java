86835eefcb664dc516b947be8b39f098b0910123
Codey Walker <codeyw@mtu.edu>
2017-03-10 18:56:26 -0500

    winBox

---
+++ Platformer/src/winBox.java
@@ -0,0 +1,9 @@
+import javafx.scene.image.ImageView;
+
+public class winBox extends Nonmoveable {
+	public winBox(double ex, double why, int sizeX, int sizeY, ImageView person, double hbx, double hby, int hbwidth,
+			int hblength) {
+		super(ex, why, sizeX, sizeY, person, hbx, hby, hbwidth, hblength);
+	}
+
+}
