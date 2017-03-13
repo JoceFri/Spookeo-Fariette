fe719ec6095aea1366b979cab3886175b1c2a4e4
Codey Walker <codeyw@mtu.edu>
2017-03-13 19:20:30 -0400

    Added Frame Setter class

---
+++ Platformer/src/FrameSetter.java
@@ -0,0 +1,36 @@
+import DavidMohrhardt.animator.Animator;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+
+public class FrameSetter {
+
+	int spriteCount = 0;
+	int cooldown = 0;
+	
+	// Sets the frames of each animation to look smooth at 60 fps
+	public FrameSetter (int numSprites) {
+		spriteCount = numSprites;
+		cooldown = 60/spriteCount;
+	}
+	
+	public Image getFrame(Animator anim, String action) {
+		
+		Image temp = null;
+		
+		if (cooldown == 0) {
+			temp = SwingFXUtils.toFXImage(anim.getNextFrame(), null);
+			cooldown = 60 / spriteCount;
+		}
+		else {
+			
+			temp = SwingFXUtils.toFXImage(anim.getFrameAtIndex(action, anim.getCurrentFrameIndex()), null);
+			cooldown--;	
+		}
+		return temp;
+	}
+	
+	public void changeCount(int count){
+		spriteCount = count;
+	}
+	
+}
