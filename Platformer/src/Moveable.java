5775cd701f0039d41bd1186fb667aa8974d37cee
cwalker97 <collinw@mtu.edu>
2017-03-26 19:27:28 -0400

    stuff

--- Platformer/src/Moveable.java
+++ Platformer/src/Moveable.java
@@ -1,9 +1,26 @@
 import javafx.scene.image.ImageView;
 
 public class Moveable extends Obj {
-	
+	boolean top = false;
+	boolean right = false;
+	boolean left = false;
+	boolean bottom = false;
 	public Moveable(double ex, double why, int sizeX, int sizeY, ImageView person, double hbx, double hby, int hbwidth,
 			int hblength) {
 		super(ex, why, sizeX, sizeY, person, hbx, hby, hbwidth, hblength);
 	}
+	public boolean getTop(){
+		return top;
+	}
+	
+	public void setTop(boolean newTop){
+		top = newTop;
+	}
+	public void resetCollision() {
+		top = false;
+		right = false;
+		left = false;
+		bottom = false;
+	}
 }
+
