333bd5e02efb3c8a8a754aaa923d633909005948
hikari1734 <mlwalthe@mtu.edu>
2017-04-10 19:20:08 -0400

    Update Moveable.java

--- Platformer/src/Moveable.java
+++ Platformer/src/Moveable.java
@@ -1,3 +1,4 @@
+import DavidMohrhardt.animator.Animator;
 import javafx.scene.image.ImageView;
 
 public class Moveable extends Obj {
@@ -10,6 +11,13 @@ public class Moveable extends Obj {
 			int hblength) {
 		super(ex, why, sizeX, sizeY, person, hbx, hby, hbwidth, hblength);
 	}
+	public Moveable(double ex, double why, int sizeX, int sizeY, double hbx, double hby, int hbwidth, int hblength) {
+		super(ex, why, sizeX, sizeY, hbx, hby, hbwidth, hblength);	}
+	public Moveable(double ex, double why, int sizeX, int sizeY, ImageView person, double hbx, double hby, int hbwidth,
+			int hblength, Animator enemy, FrameSetter enemySetter) {
+		super(ex, why, sizeX, sizeY, hbx, hby, person, hbwidth, hblength, enemy, enemySetter);
+	}
+
 	public boolean getTop(){
 		return top;
 	}
