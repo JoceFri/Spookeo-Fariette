f6b513827d73e345f46987e07eec7cf087a25110
hikari1734 <mlwalthe@mtu.edu>
2017-04-10 19:20:42 -0400

    Update Actor.java

--- Platformer/src/Actor.java
+++ Platformer/src/Actor.java
@@ -1,3 +1,4 @@
+import DavidMohrhardt.animator.Animator;
 import javafx.scene.image.ImageView;
 
 public class Actor extends Moveable {	
@@ -5,5 +6,10 @@ public class Actor extends Moveable {
 	public Actor(double ex, double why, int sizeX, int sizeY, ImageView person, double hbx, double hby, int hbwidth,
 			int hblength) {
 		super(ex, why, sizeX, sizeY, person, hbx, hby, hbwidth, hblength);
+	}
+
+	public Actor(double ex, double why, int sizeX, int sizeY, ImageView person, double hbx, double hby, int hbwidth,
+			int hblength, Animator enemy, FrameSetter enemySetter) {
+		super(ex, why, sizeX, sizeY, person, hbx, hby, hbwidth, hblength, enemy, enemySetter);
 	}
 }
