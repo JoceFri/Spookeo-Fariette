b06ad5cbcf35ed2cb8f3b3bcf245b97bf5804b76
Codey Walker <codeyw@mtu.edu>
2017-04-12 09:56:13 -0400

    Updated Rock to accommodate map loader

--- Platformer/src/Rock.java
+++ Platformer/src/Rock.java
@@ -2,9 +2,9 @@ import javafx.scene.image.ImageView;
 
 public class Rock extends Moveable {
 
-	public Rock(double ex, double why, int sizeX, int sizeY, ImageView person, double hbx, double hby, int hbwidth,
+	public Rock(double ex, double why, int sizeX, int sizeY, double hbx, double hby, int hbwidth,
 			int hblength) {
-		super(ex, why, sizeX, sizeY, person, hbx, hby, hbwidth, hblength);
+		super(ex, why, sizeX, sizeY, hbx, hby, hbwidth, hblength);
 	}
 
 }
